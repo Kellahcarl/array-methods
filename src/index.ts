2ecf68c9365b9a33e14d16c1f9dfee9efd29de96
baraka caleb <65470823+pimby@users.noreply.github.com>
2023-10-30 15:55:56 +0300

    another solution

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,26 @@ const output = Friends.map((item) => {
 });
 
 console.log(output);
+
+const filterName = (friendsNames: string[]): string[] => {
+  let resultArray: string[] = []; // loop through the array to check if there is any element having a string length of four
+  let strLength: number = 4;
+  friendsNames.forEach((element) => {
+    if (element.length === strLength) {
+      // if the conditon is satisfied then return the result array with the lsit of names with 4 charaters
+      resultArray.push(element);
+    }
+  });
+  resultArray.length === 0
+    ? console.log(
+        `there were was no name(s) in ${friendsNames} that matched the criterion of four characters `
+      )
+    : "";
+
+  return resultArray; // Return the result array after the loop
+};
+
+const friends: string[] = ["James", "Marylyn", "Moses", "Anne", "Fredy"];
+
+let result = filterName(friends);
+console.log(result);
